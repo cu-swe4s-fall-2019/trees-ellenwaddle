10a4fbd51f30b5259442425e7deeabb4040e39d5
Ellen Waddle <elwa8931@colorado.edu>
2019-11-07 12:50:23 -0700

    added testing for binary trees and solved searching dilemmas

--- binary_tree.py
+++ binary_tree.py
@@ -6,33 +6,65 @@ class Node:
         self.right = right
 
 
-    def insert(root, key, value=None):
-        '''
-        function to insert a key, value pair into trees
-        '''
-        if self.key is None:
-            self.key = key
-            self.value = value
-        elif key > self.key:
-            insert(self.right, key, value)
-        elif key < self.key:
-            insert(self.left, key, value)
+def insert(root, key, value=None):
+    try:
+        float(key)
+        if root is None:
+            root = Node(key, value=value)
+            return root
         else:
-            return 'insertion failed: is your key numeric?'
+            if key < root.key:
+                if root.left is None:
+                    root.left = Node(key, value=value)
+                else:
+                    insert(root.left, key, value=value)
+            else:
+                if root.right is None:
+                    root.right = Node(key, value=value)
+                else:
+                    insert(root.right, key, value=value)
+            return root
+    except:
+        return('your key should be numeric, not a string')
 
 
-    def search(root, key):
+def search(root, key):
         '''
-        function to search keys in a tree
+        function to search keys in a tree. will return a value associatied w
+        that key
         '''
-        if self.key == key:
-            return self.value
-        elif key < self.key:
-            search(self.left, key)
-        elif key > self.key:
-            search(self.right, key)
-        else:
-            return -1
+        if root is None:
+            return root
+
+        elif root.key == key:
+            return root
+
+        elif key > root.key:
+            if root.right is None:
+                return 'key not found'
+            else:
+                return search(root.right, key)
+
+        elif key < root.key:
+            if root.left is None:
+                return 'key not found'
+            else:
+                return search(root.left, key)
 
 if __name__ == '__main__':
-    Node()
+    '''
+    practicing with some insertions and searches
+    '''
+    root = None
+    root = insert(root, 10, 40)
+    root = insert(root, 9, 1)
+    #print('root.key', root.key)
+    #print('root.value', root.value )
+    root = insert(root, 15, 1)
+    root = insert(root, 11, 4)
+    n = search(root, 11)
+    print(n.value)
+
+    #print(root.key, root.value)
+    #print(root.left.key, root.left.value)
+    #print(root.right.left.key)
--- test_binary_tree.py
+++ test_binary_tree.py
@@ -1,28 +1,40 @@
 import unittest
 import binary_tree as bt
-
+import random
 
 '''unit testing for binary trees'''
 
 class TestBinaryTrees(unittest.TestCase):
+    def test_key_is_inserted_root_is_none(self):
+        root = None
+        root = bt.insert(root, 11, 1)
+        self.assertEqual(root.value, 1)
+
+    def test_insert_actually_inserts_on_correct_side(self):
+        root = None
+        root = bt.insert(root, 11, 1)
+        root = bt.insert(root, 5, 5)
+        self.assertEqual(root.left.value, 5)
+
+    def test_can_search(self):
+        root = None
+        root = bt.insert(root, 11, 1)
+        root = bt.insert(root, 5, 6)
+        root = bt.insert(root, 42, 21)
+        s = bt.search(root, 5)
+        self.assertEqual(s.value, 6)
+
     def test_key_is_string(self):
-        result = bt.insert(12, 'ellen')
-        self.assertEqual(result, 'insertion failed: is your key numeric?')
 
-    def test_insert_actually_inserts(self):
-        root = 10
-        key = 5
-        value = 5
-        result = bt.insert(root, key, value)
-        self.assertNotEqual(result, 'insertion failed: is your key numeric?')
+        root = None
+        result = bt.insert(root, 'ellen', 12)
+        self.assertEqual(result, 'your key should be numeric, not a string')
 
-    def test_search_finds_inserted_value(self):
-        root = 10
-        key = 5
-        value = 5
-        insert = bt.insert(root, key, value)
-        s = bt.search(root, key)
-        self.assertNotEqual(s, -1)
+    def test_search_cannot_find_value(self):
+        root = None
+        root = bt.insert(root, 102, 129)
+        s = bt.search(root, 1000)
+        self.assertEqual(s, 'key not found')
 
 if __name__ == '__main__':
     unittest.main()
