b1ba489ab68c713102f29a7fb46f28e38ed2d054
Ellen Waddle <elwa8931@colorado.edu>
2019-11-08 19:03:30 -0700

    forgot to fully update branch before pushing to master!!!

--- create_txt_files.py
+++ create_txt_files.py
@@ -4,9 +4,9 @@ import sys
 
 r = []
 for i in range(0,10000):
-    x = str(random.randint(1,10000))
+    x = str(random.randint(1,100000000))
     r.append(x)
-rfile = open('random_vals.txt', 'w')
+rfile = open('random_vals_no_repeats.txt', 'w')
 for i in r:
     rfile.write(i + "\n")
 rfile.close()
--- insert_key_value_pairs.py
+++ insert_key_value_pairs.py
@@ -15,13 +15,14 @@ pairs. both have 10,000 pairs.
 database.
 
 '''
-
-#mport hash-tables-ellenwaddle as ht
-import avl_tree
+from avl_tree import avl as Ryan_avl
+import avl as avl
+from hash-tables-ellenwaddle import hash_tables
+from hash-tables-ellenwaddle import hash_functions
 import binary_tree as bt
 import argparse
 import sys
-
+import time
 
 '''
 first thing to do is to parse dataset into key/value pairs.
@@ -54,25 +55,93 @@ if __name__ == '__main__':
         pair = l.rstrip().split('\t')
         pairs.append(pair)
         pairs = pairs[0:args.number_of_keys]
+    #print(pairs)
 
-    if args.struct == 'tree':
-        '''insert in all keys / values to an avl tree'''
-        t0_insert = time.time()
-        root = None
         keys = []
         values = []
         for i in pairs:
-            k = l.rstrip().split()
-            keys.append(k[0])
-            values.append(k[1])
-        print(keys)
+            '''
+            turns keys/values into parallel arrays
+            '''
+            #print(i)
+            p = i[0:1]
+            #print(p)
+            #key, value = p.split(",")
+            key = i[0]
+            value = i[1]
+            keys.append(key)
+            values.append(value)
+
+    t0_insert = time.time()
 
 
+    if args.struct == 'tree':
+        '''insert in all keys / values to an avl tree'''
+
+        #print(keys)
+        #print(values)
+        root = None
+        for k in keys:
+            try:
+                k = int(k)
+                root = bt.insert(root, k)
+            except:
+                print('your keys are not all numeric')
+                raise TypeError
         t1_insert = time.time()
         insert_time = t1_insert - t0_insert
-        #print(insert_time, ())
+        print('insert time', insert_time)
         '''find all keys / values to an avl tree'''
+        t0_search = time.time()
 
+        for i in keys:
+            n = bt.search(root, 11)
 
+        t1_search= time.time()
+        total_time = t1_search - t0_insert
+        print('search time', t1_search-t0_search)
+        print('total time', total_time)
+
+    if args.struct == 'AVL':
+        tree = avl.avltree()
+        '''insert'''
+        t0_insert = time.time()
+        for k in keys:
+            tree.insert(key)
+        t1_insert = time.time()
+        '''
+        search
+        '''
+        t0_search = time.time()
+
+        for k in keys:
+            tree.inorder_traverse()
+
+        t1_search = time.time()
+        print('insert time', t1_insert- t0_insert)
+        print('search time', t1_search-t0_search)
+        print('total time', t1_search - t0_insert)
+
+    if args.struct == 'hash':
+        ht = hash_tables
+        #key = 20
+        #sum = 101*2 + 108*2 + 110  # ascii sum of 'ellen'
+        #loc = sum % key
+        t0_insert = time.time()
+        for k in keys:
+            ht.add(k)
+        t0_insert = time.time()
+
+        t0_search = time.time()
+        for k in keys:
+            ht.search(k)
+        t1_search = time.time()
+
+        print('insert time', t1_insert- t0_insert)
+        print('search time', t1_search-t0_search)
+        print('total time', t1_search - t0_insert)
 
 # python insert_key_value_pairs.py --struct tree --dataset small_database.txt --number_of_keys 10
+# python insert_key_value_pairs.py --struct tree --dataset rand.txt --number_of_keys 1000
+# python insert_key_value_pairs.py --struct AVL --dataset random_vals_no_repeats.txt --number_of_keys 1000
+# python insert_key_value_pairs.py --struct hash --dataset random_vals_no_repeats.txt --number_of_keys 1000
